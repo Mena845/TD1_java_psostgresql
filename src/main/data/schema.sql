008de97c71d160b64c52f6c262abb80adc760c6c
mena <tsikiniainatsilavina@gmail.com>
2025-12-16 18:05:11 +0300

    creation des table product et product_category

---
+++ src/main/data/schema.sql
@@ -0,0 +1,13 @@
+create table Product (
+    id SERIAL primary key,
+    name varchar(200) not null ,
+    price numeric (10,2) not null ,
+    creation_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Product_category(
+    id SERIAL primary key,
+    name varchar(200) not null ,
+    product_id int not null ,
+    constraint fk_product foreign key (product_id) references Product(id) on delete cascade
+);
