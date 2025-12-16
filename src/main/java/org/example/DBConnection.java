7f0c4734efd8543806b5b759960864061a28ce4c
mena <tsikiniainatsilavina@gmail.com>
2025-12-16 19:04:04 +0300

    Ajout des class product et category

---
+++ src/main/java/org/example/DBConnection.java
@@ -0,0 +1,14 @@
+package org.example;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/product_mangement_db";
+    private static final String USER = "product_manager_user";
+    private static final String PASSWORD = "123456";
+
+    public static Connection getDBConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import java.time.Instant;
+import java.util.List;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private Instant creationDatetime;
+    private List<Category> categories;
+
+    public Product(int id, String name, double price, Instant creationDatetime, List<Category> categories) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.creationDatetime = creationDatetime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Instant getCreationDatetime() {
+        return creationDatetime;
+    }
+
+    public void setCreationDatetime(Instant creationDatetime) {
+        this.creationDatetime = creationDatetime;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+}
