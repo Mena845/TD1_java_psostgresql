4b5651f5f66e27473cb30e09a79547b23f20eab8
mena <tsikiniainatsilavina@gmail.com>
2025-12-18 09:55:54 +0300

    Debuger les privileges

--- src/main/data/init_db.sql
+++ src/main/data/init_db.sql
@@ -1,6 +1,8 @@
 -- cree la base de donne
 create database product_management_db;
 
+-- se connecter au database
+\c product_management_db
 -- cree l'User
 create user "product_manager_user" with password '123456';
 
@@ -16,7 +18,14 @@ grant create on schema public to product_manager_user;
 -- donner les droit CRUD sur tous les tables
 grant select , insert , update , delete on all tables in schema public to product_manager_user;
 
-GRANT ALL PRIVILEGES ON all tables in schema public to product_manager_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE product_category TO product_manager_user;
+
 
 select * from product;
+
+SELECT current_database();
+
+
+
+
 
